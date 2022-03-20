0b6205d0ebbade7b3da56e1dfaa9f5b13a1b43f1
337547038 <337547038@qq.com>
2022-03-20 17:32:25 +0800

    update demo

---
+++ vue-form-design/assets/test.9a5b7eea.js
@@ -0,0 +1,1 @@
+import{Z as s,o,v as e}from"./vendor.e07f640d.js";const a={class:"demo-input-suffix"};var t=s({setup:s=>(console.log(JSON.parseObject()),(s,t)=>(o(),e("div",a)))});export{t as default};
